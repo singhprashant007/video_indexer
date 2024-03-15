0e80a6c20f865767dbd740183c78549bd1807563
Prashant Singh <singhprashant0824@gmail.com>
2024-03-15 12:16:49 +0530

    summary

--- .env/app.py
+++ .env/app.py
@@ -1,20 +1,79 @@
-import sys
 import os
+import urllib
 from azure.core.credentials import AzureKeyCredential
 from azure.search.documents import SearchClient
+from langchain_community.llms import OpenAIChat
+from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
+from langchain_community.chat_models import AzureChatOpenAI
+from langchain_openai import ChatOpenAI
+import tiktoken
+from langchain.chains.summarize import load_summarize_chain
+import textwrap
+from time import monotonic
+from langchain.docstore.document import Document
+from langchain.text_splitter import CharacterTextSplitter
 from dotenv import load_dotenv
 import locale
 locale.getpreferredencoding = lambda: "UTF-8"
 load_dotenv()
 
 
-search_client = SearchClient(endpoint='https://testaicognitivesearch1.search.windows.net', index_name="iffco_original_pre",\
-                             credential= AzureKeyCredential('6BogobGfe1c6vr67EzV6BgVB2HuIDhwcAboNznRQNlAzSeBE9FFi'))
-
-llm_name ="gpt-3.5-turbo"
-index_name = 'iffco_original_pre'
-
-AZURE_COGNITIVE_SEARCH_SERVICE_NAME = os.getenv("AZURE_COGNITIVE_SEARCH_SERVICE_NAME")
-AZURE_COGNITIVE_SEARCH_INDEX_NAME = os.getenv("AZURE_COGNITIVE_SEARCH_INDEX_NAME")
-AZURE_COGNITIVE_SEARCH_API_KEY = os.getenv("AZURE_COGNITIVE_SEARCH_API_KEY")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+use_long_text = True
+
+if use_long_text:
+  url = "https://raw.githubusercontent.com/mauricio-seiji/Dataset-news-articles-pt-br/main/ciencia%20e%20tecnologia/30.txt"
+else:
+  url = "https://raw.githubusercontent.com/mauricio-seiji/Dataset-news-articles-pt-br/main/ciencia%20e%20tecnologia/22.txt"
+
+news_article = urllib.request.urlopen(url).read().decode("utf-8")
+
+model_name = "gpt-3.5-turbo"
+llm = ChatOpenAI(temperature=0, openai_api_key=OPENAI_API_KEY, model_name=model_name)
+
+text_splitter = CharacterTextSplitter.from_tiktoken_encoder(
+    model_name=model_name
+)
+
+texts = text_splitter.split_text(news_article)
+
+docs = [Document(page_content=t) for t in texts]
+print(len(docs))
+
+
+
+def num_tokens_from_string(string: str, encoding_name: str) -> int:    
+    encoding = tiktoken.encoding_for_model(encoding_name)
+    num_tokens = len(encoding.encode(string))
+    return num_tokens
+
+num_tokens = num_tokens_from_string(news_article, model_name)
+print(num_tokens)
+
+
+prompt_template = """Write a concise summary of the following:
+
+
+{text}
+
+
+CONSCISE SUMMARY IN ENGLISH:"""
+
+
+prompt = PromptTemplate(template=prompt_template, input_variables=["text"])
+model_max_tokens = 4097
+verbose = True
+
+if num_tokens < model_max_tokens:
+  chain = load_summarize_chain(llm, chain_type="stuff", prompt=prompt, verbose=verbose)
+else:
+  chain = load_summarize_chain(llm, chain_type="map_reduce", map_prompt=prompt, combine_prompt=prompt, verbose=verbose)
+
+start_time = monotonic()
+summary = chain.run(docs)
+
+
+print(f"Chain type: {chain.__class__.__name__}")
+print(f"Run time: {monotonic() - start_time}")
+print(f"Summary: {textwrap.fill(summary, width=100)}")
